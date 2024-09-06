914fedb96b890f74883a52ae26972e0b331fc201
Guilherme Daniel <gui.dani13@gmail.com>
2024-09-06 16:32:15 -0300

    init

---
+++ aula-arvores/arvores/src/main/java/one/digitalinnovation/ArvoreBinaria.java
@@ -0,0 +1,26 @@
+package main.java.one.digitalinnovation;
+
+public class ArvoreBinaria<T extends Comparable<T>> {
+
+  private BinNo<T> raiz;
+
+  ArvoreBinaria() {
+    raiz = null;
+  }
+
+  void inserir(T conteudo) {
+    BinNo<T> novoNo = new BinNo<>(conteudo);
+    raiz = inserir(raiz, novoNo);
+  }
+
+  private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo) {
+    if (atual == null) {
+      return novoNo;
+    } else if (novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
+      atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+    } else {
+      atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+    }
+    return atual;
+  }
+}
