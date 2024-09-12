f034811d3a6d37baecf512e15af9f5b3a101f02c
Guilherme Daniel <gui.dani13@gmail.com>
2024-09-12 13:39:06 -0300

    changes

---
+++ desafio-poo-dio/desafio-poo-dio/src/main/java/br/com/digitalinnovationone/Main.java
@@ -0,0 +1,9 @@
+package br.com.digitalinnovationone;
+
+public class Main {
+    public static void Main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
