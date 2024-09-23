80357de4e7779e8734d9da9ee01018200e7f6655
Guilherme Daniel <gui.dani13@gmail.com>
2024-09-23 17:46:19 -0300

    exercicio 2

--- aula-leitura-e-escrita/src/main/java/br/com/digitalinnovationone/Main.java
+++ aula-leitura-e-escrita/src/main/java/br/com/digitalinnovationone/Main.java
@@ -1,10 +1,35 @@
 package br.com.digitalinnovationone;
 
 import java.io.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public static void receberTecladoCriarDocumento() throws IOException{
+        PrintWriter printWriter = new PrintWriter(System.out);
+        printWriter.println("Digite 3 coisas: ");
+        printWriter.flush();
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        File file = new File("Coisas.txt");
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file.getName()));
+
+        do {
+            bufferedWriter.write(line);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+            line = scanner.nextLine();
+        } while (!(line.equalsIgnoreCase("Fim")));
+
+        printWriter.close();
+        scanner.close();
+        bufferedWriter.close();
+    }
 
     public static void receberTecladoImprimirConsole() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +46,9 @@ public class Main {
         bufferedReader.close();
         bufferedWriter.close();
     }
+
     public static void main(String[] args) throws IOException {
-receberTecladoImprimirConsole();
+//        receberTecladoImprimirConsole();
+        receberTecladoCriarDocumento();
     }
 }
